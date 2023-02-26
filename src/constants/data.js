a001e9eb712bc23db5825a02c1619b6080ba534a
Filipo Marcellino <fmarcellino7@gmail.com>
2023-02-25 21:27:38 -0800

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,23 @@
+export const BIO_INFO = {
+    'name': 'Filipo Marcellino',
+    'about': 'software systems @ SFU || CS TA @ Langara',
+    'location': 'Vancouver, BC, Canada',
+    'contact': {
+        'email': 'fmarcellino7@gmail.com',
+        'github': 'github.com/filipomarcellino',
+        'linkedin': 'linkedin.com/in/filipomarcellino',
+        'instagram': 'www.instagram.com/filipomarcellino'
+    },
+    'hobbies': [
+        'soccer || futsal',
+        'gym',
+        'volleyball',
+        'badminton',
+        'table tennis',
+
+    ],
+    'interests': [
+        'anime',
+        'movies',
+    ],
+}
